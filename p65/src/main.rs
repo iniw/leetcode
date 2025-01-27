13e219978327f60ae77985539f90f829b969696d
Vinicius Deolindo <git@vini.cat>
2025-01-27 08:39:04 -0300

    p65

---
+++ p65/src/main.rs
@@ -0,0 +1,73 @@
+use std::{iter::Peekable, str::Chars};
+
+impl Solution {
+    pub fn is_number(s: String) -> bool {
+        let mut s = s.trim().chars().peekable();
+
+        Self::consume_sign(&mut s);
+
+        let found = match s.next() {
+            Some('.') => match s.peek() {
+                Some('0'..='9') => Found::Dot,
+                _ => return false,
+            },
+            Some('0'..='9') => {
+                Self::consume_numbers(&mut s);
+                match s.next() {
+                    Some('.') => Found::Dot,
+                    Some('e' | 'E') => Found::Exponent,
+                    Some(_) => return false,
+                    None => return true,
+                }
+            }
+            _ => return false,
+        };
+
+        match found {
+            Found::Dot => match s.next() {
+                Some('e' | 'E') => Self::consume_after_exponent(&mut s),
+                Some('0'..='9') => {
+                    Self::consume_numbers(&mut s);
+                    match s.next() {
+                        Some('e' | 'E') => Self::consume_after_exponent(&mut s),
+                        Some(_) => false,
+                        None => true,
+                    }
+                }
+                Some(_) => false,
+                None => true,
+            },
+            Found::Exponent => Self::consume_after_exponent(&mut s),
+        }
+    }
+
+    fn consume_numbers(s: &mut Peekable<Chars>) -> bool {
+        let mut found = false;
+        while s.next_if(|c| c.is_numeric()).is_some() {
+            found = true;
+        }
+        found
+    }
+
+    fn consume_after_exponent(s: &mut Peekable<Chars>) -> bool {
+        Self::consume_sign(s);
+        Self::consume_numbers(s) && s.next().is_none()
+    }
+
+    fn consume_sign(s: &mut Peekable<Chars>) {
+        s.next_if(|c| *c == '-' || *c == '+');
+    }
+}
+
+enum Found {
+    Dot,
+    Exponent,
+}
+
+struct Solution;
+
+fn main() {
+    use std::env;
+    let n = env::args().nth(1).expect("input");
+    println!("is_number = {}", Solution::is_number(n));
+}
